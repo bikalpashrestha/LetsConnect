94bbc50b625001c3e989d00f0646610617a769ee
bikalpashrestha <bikalpashrestha357@gmail.com>
2022-05-27 16:16:19 +0545

    follow

---
+++ frontend/src/redux/actions/profileAction.js
@@ -0,0 +1,39 @@
+import { GLOBALTYPES, DeleteData } from './globalTypes'
+import { getDataAPI, patchDataAPI } from '../../utils/fetchData'
+import { imageUpload } from '../../utils/imageUpload'
+import { createNotify, removeNotify } from '../actions/notifyAction'
+
+
+export const PROFILE_TYPES = {
+    
+    FOLLOW: 'FOLLOW',
+
+}
+
+
+
+
+
+export const follow = ({users, user, auth, socket}) => async (dispatch) => {
+    let newUser;
+    
+    if(users.every(item => item._id !== user._id)){
+        newUser = {...user, followers: [...user.followers, auth.user]}
+    }else{
+        users.forEach(item => {
+            if(item._id === user._id){
+                newUser = {...item, followers: [...item.followers, auth.user]}
+            }
+        })
+    }
+
+    dispatch({ type: PROFILE_TYPES.FOLLOW, payload: newUser })
+
+    dispatch({
+        type: GLOBALTYPES.AUTH, 
+        payload: {
+            ...auth,
+            user: {...auth.user, following: [...auth.user.following, newUser]}
+        }
+    })
+
