31e711049d06a66cabcb688a002e80147c0daca7
bigyan815 <bigyantamang.90@gmail.com>
2022-05-29 23:17:40 +0545

    User id completed

--- frontend/src/pages/profile/[id].js
+++ frontend/src/pages/profile/[id].js
@@ -21,4 +21,35 @@ const Profile = () => {
         if(profile.ids.every(item => item !== id)){
             dispatch(getProfileUsers({id, auth}))
         }
-   
+    },[id, auth, dispatch, profile.ids])
+
+    return (
+        <div className="profile">
+            
+            <Info auth={auth} profile={profile} dispatch={dispatch} id={id} />
+
+            {
+                auth.user._id === id &&
+                <div className="profile_tab">
+                    <button className={saveTab ? '' : 'active'} onClick={() => setSaveTab(false)}>Posts</button>
+                    <button className={saveTab ? 'active' : ''} onClick={() => setSaveTab(true)}>Saved</button>
+                </div>
+            }
+
+            {
+                profile.loading 
+                ? <img className="d-block mx-auto" src={LoadIcon} alt="loading" />
+                : <>
+                    {
+                        saveTab
+                        ? <Saved auth={auth} dispatch={dispatch} />
+                        : <Posts auth={auth} profile={profile} dispatch={dispatch} id={id} />
+                    }
+                </>
+            }
+            
+        </div>
+    )
+}
+
+export default Profile
