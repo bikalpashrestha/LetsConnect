cb85a6cee9aba873dab2a3675d8aebbc7914e678
aairothapaliya <aairo2077@gmail.com>
2022-06-03 19:59:53 +0545

    authentication

--- frontend/src/components/profile/Posts.js
+++ frontend/src/components/profile/Posts.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import PostThumb from '../PostThumb'
+import LoadIcon from '../../images/loading.gif'
+import LoadMoreBtn from '../LoadMoreBtn'
+import { getDataAPI } from '../../utils/fetchData'
+import { PROFILE_TYPES } from '../../redux/actions/profileAction'
 
+const Posts = ({auth, id, dispatch, profile}) => {
+    const [posts, setPosts] = useState([])
+    const [result, setResult] = useState(9)
+    const [page, setPage] = useState(0)
+    const [load, setLoad] = useState(false)
 
-
+    useEffect(() => {
+        profile.posts.forEach(data => {
+            if(data._id === id){
+                setPosts(data.posts)
+                setResult(data.result)
+                setPage(data.page)
+            }
+        })
+    },[profile.posts, id])
 
     const handleLoadMore = async () => {
         setLoad(true)
