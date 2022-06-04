0ee2d9ba04565a8f941e3ce062bb7c1e37050a24
bigyan815 <bigyantamang.90@gmail.com>
2022-06-04 22:26:17 +0545

    commentmodel

---
+++ backend/models/commentModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    tag: Object,
+    reply: mongoose.Types.ObjectId,
+    postId: mongoose.Types.ObjectId,
+    postUserId: mongoose.Types.ObjectId
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('comment', commentSchema)
