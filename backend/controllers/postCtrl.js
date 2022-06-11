f4a785dfd8de96c545da47b28f072a2f3747a5d3
bigyan815 <bigyantamang.90@gmail.com>
2022-06-11 10:43:32 +0545

    save post backend

---
+++ backend/controllers/postCtrl.js
@@ -0,0 +1,70 @@
+const Posts = require('../models/postModel')
+const Comments = require('../models/commentModel')
+const Users = require('../models/userModel')
+
+class APIfeatures {
+    constructor(query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    paginating(){
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9 
+        const skip = (page - 1) * limit
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
+    }
+}
+
+    savePost: async (req, res) => {
+        try {
+            const user = await Users.find({_id: req.user._id, saved: req.params.id})
+            if(user.length > 0) return res.status(400).json({msg: "You saved this post."})
+
+            const save = await Users.findOneAndUpdate({_id: req.user._id}, {
+                $push: {saved: req.params.id}
+            }, {new: true})
+
+            if(!save) return res.status(400).json({msg: 'This user does not exist.'})
+
+            res.json({msg: 'Saved Post!'})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    unSavePost: async (req, res) => {
+        try {
+            const save = await Users.findOneAndUpdate({_id: req.user._id}, {
+                $pull: {saved: req.params.id}
+            }, {new: true})
+
+            if(!save) return res.status(400).json({msg: 'This user does not exist.'})
+
+            res.json({msg: 'unSaved Post!'})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getSavePosts: async (req, res) => {
+        try {
+            const features = new APIfeatures(Posts.find({
+                _id: {$in: req.user.saved}
+            }), req.query).paginating()
+
+            const savePosts = await features.query.sort("-createdAt")
+
+            res.json({
+                savePosts,
+                result: savePosts.length
+            })
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+}
+
+module.exports = postCtrl
