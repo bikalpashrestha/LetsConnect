6afe1bc536eaf2bd01f41a81e1f75aef500fc26d
aairothapaliya <aairo2077@gmail.com>
2022-06-11 20:25:21 +0545

    update post

--- frontend/src/redux/reducers/postReducer.js
+++ frontend/src/redux/reducers/postReducer.js
@@ -11,7 +11,34 @@ const initialState = {
 
 const postReducer = (state = initialState, action) => {
     switch (action.type){
-        
+        case POST_TYPES.CREATE_POST:
+            return {
+                ...state,
+                posts: [action.payload, ...state.posts]
+            };
+        case POST_TYPES.LOADING_POST:
+            return {
+                ...state,
+                loading: action.payload
+            };
+        case POST_TYPES.GET_POSTS:
+            return {
+                ...state,
+                posts: action.payload.posts,
+                result: action.payload.result,
+                page: action.payload.page
+            };
+        case POST_TYPES.GET_COMMENTS:
+            return {
+                ...state,
+                comments:action.payload.comments
+
+            };
+        case POST_TYPES.UPDATE_POST:
+            return {
+                ...state,
+                posts: EditData(state.posts, action.payload._id, action.payload)
+            };
         case POST_TYPES.DELETE_POST:
             return {
                 ...state,
