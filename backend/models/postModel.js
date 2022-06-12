a6d90a3ee6a9c25cb5b88c3307c1d21dc4383104
bigyan815 <bigyantamang.90@gmail.com>
2022-06-12 22:46:04 +0545

    postModel

---
+++ backend/models/postModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    content: String,
+    images: {
+        type: Array,
+        required: true
+    },
+    user: {type: mongoose.Types.ObjectId, ref: 'user'}
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('post', postSchema)
