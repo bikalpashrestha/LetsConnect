cb33f04cb2a03e6b8beed8b2a980fd2cb7447fd9
bikalpashrestha <bikalpashrestha357@gmail.com>
2022-06-15 23:54:43 +0545

    discover page

---
+++ backend/routes/postRouter.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const postCtrl = require('../controllers/postCtrl')
+const auth = require('../middleware/auth')
+
+router.route('/posts')
+    .post(auth, postCtrl.createPost)
+    .get(auth, postCtrl.getPosts)
+
+
+
+router.get('/post_discover', auth, postCtrl.getPostsDicover)
+
+
+module.exports = router
