31478034457acee9980d98e13e1c8053e3ebd0f7
aairothapaliya <aairo2077@gmail.com>
2022-06-17 20:29:40 +0545

    reply comment

--- backend/controllers/commentCtrl.js
+++ backend/controllers/commentCtrl.js
@@ -43,51 +43,6 @@ const commentCtrl = {
             return res.status(500).json({ msg: err.message })
         }
     },
-const Comments = require('../models/commentModel')
-const Posts = require('../models/postModel')
-
-const commentCtrl = {
-    createComment: async (req, res) => {
-        try {
-            const { postId, content, tag, reply, postUserId } = req.body
-
-            const post = await Posts.findById(postId)
-            if (!post) return res.status(400).json({ msg: "This post does not exist." })
-
-            if (reply) {
-                const cm = await Comments.findById(reply)
-                if (!cm) return res.status(400).json({ msg: "This comment does not exist." })
-            }
-
-            const newComment = new Comments({
-                user: req.user._id, content, tag, reply, postUserId, postId
-            })
-
-            await Posts.findOneAndUpdate({ _id: postId }, {
-                $push: { comments: newComment._id }
-            }, { new: true })
-
-            await newComment.save()
-            res.json({ newComment })
-
-        } catch (err) {
-            return res.status(500).json({ msg: err.message })
-        }
-    },
-    updateComment: async (req, res) => {
-        try {
-            const { content } = req.body
-
-            await Comments.findOneAndUpdate({
-                _id: req.params.id, user: req.user._id
-            }, { content })
-
-            res.json({ msg: 'Update Success!' })
-
-        } catch (err) {
-            return res.status(500).json({ msg: err.message })
-        }
-    },
     likeComment: async (req, res) => {
         try {
             const comment = await Comments.find({ _id: req.params.id, likes: req.user._id })
@@ -151,4 +106,3 @@ const commentCtrl = {
 
 
 module.exports = commentCtrl
-    
