80ef3c0ae27f690e679e8b6555aa05092e9619ba
aairothapaliya <aairo2077@gmail.com>
2022-06-24 20:36:58 +0545

    real time function

--- frontend/src/redux/actions/profileAction.js
+++ frontend/src/redux/actions/profileAction.js
@@ -60,3 +60,74 @@ export const updateProfileUser = ({userData, avatar, auth}) => async (dispatch) 
             ...userData,
             avatar: avatar ? media[0].url : auth.user.avatar
         }, auth.token)
+ try {
+        const res = await patchDataAPI(`user/${user._id}/follow`, null, auth.token)
+        socket.emit('follow', res.data.newUser)
+
+        // Notify
+        const msg = {
+            id: auth.user._id,
+            text: 'has started to follow you.',
+            recipients: [newUser._id],
+            url: `/profile/${auth.user._id}`,
+        }
+
+        dispatch(createNotify({msg, auth, socket}))
+
+    } catch (err) {
+        dispatch({
+            type: GLOBALTYPES.ALERT, 
+            payload: {error: err.response.data.msg}
+        })
+    }
+}
+
+export const unfollow = ({users, user, auth, socket}) => async (dispatch) => {
+
+    let newUser;
+
+    if(users.every(item => item._id !== user._id)){
+        newUser = {...user, followers: DeleteData(user.followers, auth.user._id)}
+    }else{
+        users.forEach(item => {
+            if(item._id === user._id){
+                newUser = {...item, followers: DeleteData(item.followers, auth.user._id)}
+            }
+        })
+    }
+
+    dispatch({ type: PROFILE_TYPES.UNFOLLOW, payload: newUser })
+
+    dispatch({
+        type: GLOBALTYPES.AUTH, 
+        payload: {
+            ...auth,
+            user: { 
+                ...auth.user, 
+                following: DeleteData(auth.user.following, newUser._id) 
+            }
+        }
+    })
+   
+
+    try {
+        const res = await patchDataAPI(`user/${user._id}/unfollow`, null, auth.token)
+        socket.emit('unFollow', res.data.newUser)
+
+        // Notify
+        const msg = {
+            id: auth.user._id,
+            text: 'has started to follow you.',
+            recipients: [newUser._id],
+            url: `/profile/${auth.user._id}`,
+        }
+
+        dispatch(removeNotify({msg, auth, socket}))
+
+    } catch (err) {
+        dispatch({
+            type: GLOBALTYPES.ALERT, 
+            payload: {error: err.response.data.msg}
+        })
+    }
+}
