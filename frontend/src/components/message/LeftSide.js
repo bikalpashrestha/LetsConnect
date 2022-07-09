d7260b134036eed97d77587db03667a254ab8815
aairothapaliya <aairo2077@gmail.com>
2022-07-09 20:54:19 +0545

    message

--- frontend/src/components/message/LeftSide.js
+++ frontend/src/components/message/LeftSide.js
@@ -1,13 +1,38 @@
 import React, { useState, useEffect, useRef } from 'react'
-import UserCard from '../UserCard'
-i
+import { useSelector, useDispatch } from 'react-redux'
+import { getDataAPI } from '../../utils/fetchData'
+import { GLOBALTYPES } from '../../redux/actions/globalTypes'
+import { useHistory, useParams } from 'react-router-dom'
+import { MESS_TYPES, getConversations } from '../../redux/actions/messageAction'
 
 const LeftSide = () => {
     const { auth, message, online } = useSelector(state => state)
-   
+   const dispatch = useDispatch()
+
+    const [search, setSearch] = useState('')
+    const [searchUsers, setSearchUsers] = useState([])
+
+    const history = useHistory()
+    const { id } = useParams()
+
+    const pageEnd = useRef()
 
 
     
+    const handleSearch = async e => {
+        e.preventDefault()
+        if(!search) return setSearchUsers([]);
+
+        try {
+            const res = await getDataAPI(`search?username=${search}`, auth.token)
+            setSearchUsers(res.data.users)
+        } catch (err) {
+            dispatch({
+                type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}
+            })
+        }
+    }
+
     
 
    
