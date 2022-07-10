506dd2f7c05156fff6acc580a95b0d6281d3ec58
aairothapaliya <aairo2077@gmail.com>
2022-07-10 20:54:55 +0545

    message

--- frontend/src/components/message/LeftSide.js
+++ frontend/src/components/message/LeftSide.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react'
+import UserCard from '../UserCard'
 import { useSelector, useDispatch } from 'react-redux'
 import { getDataAPI } from '../../utils/fetchData'
 import { GLOBALTYPES } from '../../redux/actions/globalTypes'
 import { useHistory, useParams } from 'react-router-dom'
 import { MESS_TYPES, getConversations } from '../../redux/actions/messageAction'
 
+
 const LeftSide = () => {
     const { auth, message, online } = useSelector(state => state)
-   const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
     const [search, setSearch] = useState('')
     const [searchUsers, setSearchUsers] = useState([])
@@ -33,9 +35,32 @@ const LeftSide = () => {
         }
     }
 
+    const handleAddUser = (user) => {
+        setSearch('')
+        setSearchUsers([])
+        dispatch({type: MESS_TYPES.ADD_USER, payload: {...user, text: '', media: []}})
+        dispatch({type: MESS_TYPES.CHECK_ONLINE_OFFLINE, payload: online})
+        return history.push(`/message/${user._id}`)
+    }
+
+    const isActive = (user) => {
+        if(id === user._id) return 'active';
+        return ''
+    }
+
+    useEffect(() => {
+        if(message.firstLoad) return;
+        dispatch(getConversations({auth}))
+    },[dispatch, auth, message.firstLoad])
     
 
-   
+    // Check User Online - Offline
+    useEffect(() => {
+        if(message.firstLoad) {
+            dispatch({type: MESS_TYPES.CHECK_ONLINE_OFFLINE, payload: online})
+        }
+    },[online, message.firstLoad, dispatch])
+
     return (
         <>
             <form className="message_header" onSubmit={handleSearch} >
@@ -59,7 +84,27 @@ const LeftSide = () => {
                             ))
                         }
                         
-                    
+                    </>
+                    : <>
+                        {
+                            message.users.map(user => (
+                                <div key={user._id} className={`message_user ${isActive(user)}`}
+                                onClick={() => handleAddUser(user)}>
+                                    <UserCard user={user} msg={true}>
+                                        {
+                                            user.online
+                                            ? <i className="fas fa-circle text-success" />
+                                            : auth.user.following.find(item => 
+                                                item._id === user._id
+                                            ) && <i className="fas fa-circle" />
+                                                
+                                        }
+                                        
+                                    </UserCard>
+                                </div>
+                            ))
+                        }
+                    </>
                 }
                
                <button ref={pageEnd} style={{opacity: 0}} >Load More</button>
