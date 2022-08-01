fe1bff64c7d2564b6dc06e183f01a61c073a835d
aairothapaliya <aairo2077@gmail.com>
2022-08-01 19:41:54 +0545

    auth

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 25
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 25,
+        unique: true 
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar:{
+        type: String,
+        default: 'https://res.cloudinary.com/sabin149/image/upload/v1644998344/avatar/avatar_vhonwz.png'
+    },
+module.exports = mongoose.model('user', userSchema)
